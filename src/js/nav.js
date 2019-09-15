577d5c7f3cfe6bae385571158e571cc590eefc4e
DavidCertinergie <david@certinergie.be>
2019-09-15 10:02:50 +0200

    :sparkles: Create landing page for new website

---
+++ src/js/nav.js
@@ -0,0 +1,15 @@
+// $(function() {
+//   $(window).scroll(function() {
+//     var mainHeaderHeight = $("#main-header").height();
+//     if ($(this).scrollTop() > mainHeaderHeight) {
+//       $("#main-header").addClass("scroll");
+//       console.log(mainHeaderHeight);
+//     } else {
+//       $("#main-header").removeClass("scroll");
+//     }
+//   });
+//   $("#nav-trigger").click(function() {
+//     $(this).toggleClass("open");
+//     $("#main-navigation").toggleClass("open");
+//   });
+// });
---
+++ src/js/smooth-scroll.js
@@ -0,0 +1,16 @@
+// $(function() {
+//   $('a[href^="#"]').click(function() {
+//     var hash = $(this).attr("href");
+//     var mainHeaderHeight = $("#main-header").height();
+//     if (hash === "#") {
+//       return;
+//     }
+//     $("html, body").animate(
+//       {
+//         scrollTop: $(hash).offset().top - mainHeaderHeight
+//       },
+//       "slow"
+//     );
+//     return false;
+//   });
+// });
